7beb614978cb43641278f17f7f7c3fcac9dcdc1f
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-04 16:28:19 +0100

    Genero 5 numeri con un ciclo e li metto nell'array

--- 1/myscript.js
+++ 1/myscript.js
@@ -1,14 +1,21 @@
 // Un alert espone 5 numeri casuali. Da li parte un timer di 30 secondi.
 // Dopo 30 secondi l’utente deve inserire un prompt alla volta i numeri che ha visto precedentemente. Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati;
 
-// genero 5 numeri casuali
-
-var numero = numRandom(1,1000); // invoco la funzione (da 1 a 1000 per comodità)
-console.log(numero);
-
-// li metto in un array
+// genero 5 numeri casuali e li metto in un array
 
 var daIndovinare = []; // dispongo l'array vuoto
+
+for (i = 0; i < 5; i++){
+  var numero = numRandom(1,1000); // invoco la funzione (da 1 a 1000 per comodità)
+  console.log(numero);
+  daIndovinare.push(numero);
+}
+console.log(daIndovinare);
+
+
+
+
+
 
 // li metto in un alert
 
