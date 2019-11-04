f60034f192682f4a5ecd6356458c11e84a26f9ad
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-04 16:13:24 +0100

    pseudocodice

---
+++ 1/myscript.js
@@ -0,0 +1,14 @@
+// Un alert espone 5 numeri casuali. Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l’utente deve inserire un prompt alla volta i numeri che ha visto precedentemente. Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati;
+
+// genero 5 numeri casuali
+// li metto in un array
+
+// li metto in un alert
+
+// faccio passare 30 secondi
+
+// faccio comparire 5 prompt
+// metto questi 5 numeri in un array
+
+// vedo quali e quanti sono uguali
